28b9a75bc3d0a4b2ac5bbe1666278fa325cf7901
StefanyaSM <stefanyasalm@gmail.com>
2024-05-25 22:49:57 -0700

    except
    intento de agregar except

--- search/searchFun.py
+++ search/searchFun.py
@@ -6,8 +6,13 @@ def search(employees, employeeToSearch):
     #         print(f"Last Name: {employee['lastName']}")
     #         print(f"Email: {employee['email']}")
     #         print(f"{employee['seniority']} years seniority")
-    
-    print(f"Name: {employees[employeeToSearch]['name']}")
-    print(f"Last Name: {employees[employeeToSearch]['lastName']}")
-    print(f"Email: {employees[employeeToSearch]['email']}")
-    print(f"{employees[employeeToSearch]['seniority']} years seniority")
+    try:
+        print(f"Name: {employees[employeeToSearch]['name']}")
+        print(f"Last Name: {employees[employeeToSearch]['lastName']}")
+        print(f"Email: {employees[employeeToSearch]['email']}")
+        print(f"{employees[employeeToSearch]['seniority']} years seniority")
+        
+    except EOFError:
+        print(colored(" ╔══════════════════════════╗ ","red"))
+        print(colored(" ║     Error: Void list     ║  ","red"))
+        print(colored(" ╚══════════════════════════╝ ","red"))
